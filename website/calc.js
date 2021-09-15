734df77ae766bb890391c9f2f7a84b424752e23b
harshada <harshada.kadam@ambab.com>
2021-09-15 12:46:50 +0530

    modified Html javascrit,jquery assignment

--- website/calc.js
+++ website/calc.js
@@ -42,3 +42,57 @@ function mod() {
     document.getElementById('answer').innerHTML = add;
 }
 
+//Jquery
+
+$( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true,
+      active: false
+    });
+  } );
+
+  
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
+ 
+
+ $(document).ready(function(){
+   
+   $("#add1").click(function(){
+     var a= Number($("#number1").val());
+     var b= Number($("#number2").val());
+     var c= Number(a + b);
+     $("#answer1").html(c);
+   });
+ });
+
+ $(document).ready(function(){
+   
+   $("#sub2").click(function(){
+     var a= Number($("#number1").val());
+     var b= Number($("#number2").val());
+     var c= Number(a - b);
+     $("#answer1").html(c);
+   });
+ });
+
+ $(document).ready(function(){
+   
+   $("#mul3").click(function(){
+     var a= Number($("#number1").val());
+     var b= Number($("#number2").val());
+     var c= Number(a * b);
+     $("#answer1").html(c);
+   });
+ });
+
+ $(document).ready(function(){
+   
+   $("#div4").click(function(){
+     var a= Number($("#number1").val());
+     var b= Number($("#number2").val());
+     var c= Number(a / b);
+     $("#answer1").html(c);
+   });
+ });
